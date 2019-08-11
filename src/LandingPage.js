9e570c5a74b4923af2af7bbe5d6801e2cdec8e4a
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2019-08-10 23:13:50 -0500

    Diseño de landing page
    se termina el diseño del landing, faltaría solo el contenido y los enlaces

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -31,26 +31,42 @@ class LandingPage extends Component {
         const colStyle = {maxWidth: "22rem"};
 
         return (
+
             <div>
-                <MDBView src={`https://images.pexels.com/photos/669996/pexels-photo-669996.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`}>
+                {
+                    /* HEADER   */
+                }
+                <MDBView
+                    src={`https://images.pexels.com/photos/669996/pexels-photo-669996.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`}>
                     <MDBMask className="rgba-purple-slight d-flex justify-content-center align-items-center">
                         <MDBContainer>
                             <MDBRow>
-                                <MDBCol md="12" className="mb-4 text-center">
-                                    <h1 className="display-4 font-weight-bold mb-0 pt-md-5 pt-5">¿Que es kōkua?</h1>
-                                    <h5 className="pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5">Kokua viene de la palabra
+                                <MDBCol md="6" className="mb-4 text-left">
+                                    <h1 className="display-5 mb-0 " id={"heatitle"}>¿Que es kōkua?</h1>
+                                    <h5 className="pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5" id={"texthead"}>Kokua
+                                        viene de la palabra
                                         hawaiana "kōkua" que signifiica Ayuda y en esta plataforma podrás encontrar toda
                                         la ayuda necesaria para tus tareas y proyectos escolares.</h5>
-                                    <MDBBtn rounded className="btn-purple"><MDBIcon icon="user"
-                                                                                    className="mr-2"></MDBIcon> Sign up!</MDBBtn>
-                                    <MDBBtn outline color="purple" rounded><MDBIcon icon="book"
-                                                                                    className="mr-2"></MDBIcon> Learn
-                                        more</MDBBtn>
+                                    <MDBBtn rounded className="cian"><MDBIcon icon="user"
+                                                                              className="mr-2"/> Sign up!</MDBBtn>
+
+                                </MDBCol>
+                                <MDBCol md="6" className="align-content-lg-center">
+                                    <div className="embed-responsive embed-responsive-16by9">
+                                        {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+                                        <iframe className="embed-responsive-item"
+                                                src="https://www.youtube.com/embed/vlDzYIIOYmM"
+                                                align="middle"/>
+                                    </div>
                                 </MDBCol>
                             </MDBRow>
                         </MDBContainer>
                     </MDBMask>
                 </MDBView>
+
+                {
+                    /* Cursos   */
+                }
                 <section id="sectionCurInd">
                     <br/><br/>
                     <h2>Cursos</h2>
@@ -121,6 +137,9 @@ class LandingPage extends Component {
                     </MDBContainer>
                     <br/><br/>
                 </section>
+                {
+                    /* Profesres   */
+                }
                 <section className="sectionCurInd">
                     <br/><br/>
                     <h2 className="titulosIndex">Profesores</h2>
@@ -191,11 +210,12 @@ class LandingPage extends Component {
                     </MDBContainer>
                     <br/><br/>
                 </section>
-
+                {
+                    /* Footer   */
+                }
                 <MDBFooter color="purple" className="font-small pt-4 mt-4">
                     <MDBContainer className="text-center text-md-left">
-                        <br/><br/><br/><br/>
-
+                        <br/><br/>
 
                         <MDBRow>
                             <MDBCol md="3">
@@ -215,7 +235,7 @@ class LandingPage extends Component {
                                 </ul>
                             </MDBCol>
                             <MDBCol md="6">
-                                <h5 className="title">Kokua UTNG</h5>
+                                <h3 >Kokua UTNG</h3>
                                 <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut
                                     vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit
                                     pulvinar dictum vel in justo.</p>
